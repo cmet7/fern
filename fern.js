8c56ab4fefa1392e6d8a80cddb6a76f34ffc1b1f
Chris Metcalfe <chrsmtclf@gmail.com>
2012-09-23 19:36:51 -0400

    Scale changes

--- fern.js
+++ fern.js
@@ -1,3 +1,8 @@
+//	Chris Metcalfe
+//	9/23/2012
+//
+//	Barnsley Fern Fractal
+
 function colorPixel(pixelArray, x, y, r, g, b, a){
 	index = (x + y * pixelArray.width) * 4;	
 	pixelArray.data[index + 0] = r;
@@ -8,7 +13,15 @@ function colorPixel(pixelArray, x, y, r, g, b, a){
 
 $(document).ready(function() {
 
-var points = 1000000;
+var points = 500000;
+
+function sizeCanvas(){
+	var canvas = document.getElementById("myCanvas");
+	canvas.height = window.innerHeight - 8;
+	canvas.width = window.innerWidth - 16;
+}
+
+sizeCanvas();
 
 function setArray(){
 	var canvas = document.getElementById("myCanvas");
@@ -40,12 +53,9 @@ function setArray(){
   		x = 0.85 * last_x + 0.04 * last_y;
   		y = -0.04 * last_x + 0.85 * last_y + 1.6;
   	}
-		//console.log(x + " " + y);
-//		var real_x = Math.floor(x * (100 * scale)) + translatePos.x;
-//		var real_y  = Math.floor(y * 100 * scale) + translatePos.y - canvas.height / 2;
-//		if(real_x < canvas.width && real_y < canvas.height && real_x > 0 && real_y > 0) {
-//			colorPixel(pixelArray, real_x, real_y, 0, 128, 0, 255);
-//		}
+
+		// X and Y values transposed to turn the fern sideways.
+		// This fits widescreen monitors more easily.
 		valuesArray[i * 2] = x;
 		valuesArray[i * 2 + 1] = y;
 		
@@ -77,27 +87,31 @@ function draw(scale, translatePos){
 	var x = 0;
 	var y = 0;
 
+	console.log(translatePos);
+
 	for(var i = 0; i < points; i++){
 		x = valuesArray[i*2];
 		y = valuesArray[i*2 + 1];
-		var real_x = Math.floor(x * (100 * scale)) + translatePos.x;
-		var real_y  = Math.floor(y * 100 * scale) + translatePos.y - canvas.height / 2;
-		if(real_x < canvas.width && real_y < canvas.height && real_x > 0 && real_y > 0) {
-			colorPixel(pixelArray, real_x, real_y, 0, 128, 0, 255);
+		var real_x = Math.floor(x * 100 * scale) + translatePos.x;
+		var real_y  = Math.floor(y * 100 * scale) + translatePos.y;
+		
+		// Only draw a pixel if we can actually see it on the screen
+		if(real_x > 0 && real_y > 0 && real_x < canvas.width && real_y < canvas.height){
+				colorPixel(pixelArray, real_x, real_y, 0, 128, 0, 255);
 		}
 	}
 
 	context.putImageData(pixelArray, 0, 0);
 } //end draw()
 
-	var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("myCanvas");
 
-	var translatePos = {
-			x: canvas.width / 2,
-			y: canvas.height / 2
-	};
+var translatePos = {
+	x: canvas.width / 2,
+	y: 0
+};
 
-var scale = 0.9;
+var scale = canvas.height / 1080;
 var scaleMultiplier = 0.8;
 var startDragOffset = {};
 var mouseDown = false;
@@ -105,11 +119,16 @@ var mouseDown = false;
 // add button event listeners
 document.getElementById("plus").addEventListener("click", function(){
 		scale /= scaleMultiplier;
+		translatePos.x = Math.floor(translatePos.x * scaleMultiplier);
+		translatePos.y = Math.floor(translatePos.y * scaleMultiplier);
 		draw(scale, translatePos);
 }, false);
 
 document.getElementById("minus").addEventListener("click", function(){
 		scale *= scaleMultiplier;
+		translatePos.x = Math.floor(translatePos.x / scaleMultiplier);
+		translatePos.y = Math.floor(translatePos.y / scaleMultiplier);
+
 		draw(scale, translatePos);
 }, false);
 
